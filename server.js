245255dde66f56eab62139120a228fc8b636c0a9
Harshit Shukla <Harshitshukla151001@gmail.com>
2021-05-25 23:32:22 +0530

    f

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(compression());
 app.use("/carecategory", express.static(path.join(__dirname, "nutritional")));
 
-app.use("/care-category", express.static(path.join(__dirname, "digital")));
+app.use("/carecategory", express.static(path.join(__dirname, "digital")));
 
 app.get("/carecategory/nutritional-care", function (req, res) {
   res.sendFile(path.join(__dirname, "nutritional-care.html"));
@@ -18,13 +18,13 @@ app.get("/carecategory/thankyou", function (req, res) {
   res.sendFile(path.join(__dirname, "thankyou1.html"));
 });
 
-app.get("/care-category/digital", function (req, res) {
+app.get("/carecategory/digital", function (req, res) {
   res.sendFile(path.join(__dirname, "digital.html"));
 });
-app.get("/care-category/digital-literacy", function (req, res) {
+app.get("/carecategory/digital-literacy", function (req, res) {
   res.sendFile(path.join(__dirname, "digital-literacy.html"));
 });
-app.get("/care-category/thankyou", function (req, res) {
+app.get("/carecategory/thankyou", function (req, res) {
   res.sendFile(path.join(__dirname, "thankyou2.html"));
 });
 
